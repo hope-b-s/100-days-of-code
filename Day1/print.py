0ef26390f21385077e6834825edbc254494c9a7b
hopebs-gw <61190338+hopebs-gw@users.noreply.github.com>
2024-06-20 01:41:18 +0100

    Day1-Upload
    Upload work from day 1 covering Print, Input and Variables.

---
+++ Day1/print.py
@@ -0,0 +1,19 @@
+# This will print whats inside the ""
+print("1 - This prints 1 line")
+
+# This will print and the \n will create a new line
+print("2 - This prints\n2 lines")
+
+# The + allows you to combine different elements or variables e.g. add a space.
+print("One" + " " + "Two")
+
+# When you want to add a space between the two words you can add a space after the first word or before the second word
+# To use quotes in a string you can use the \ to escape the quotes, or use ' instead of "
+print("A 'single quote' inside a double quote")
+print('A "double quote" inside a single quote')
+print("Alternatively you can just \"escape\" the quote")
+
+# The input function will get the user input in the console
+print("Hello " + input("What is your name? ") + "!")
+
+# To add a comment use ctrl+/ and to remove a comment use ctrl+z
